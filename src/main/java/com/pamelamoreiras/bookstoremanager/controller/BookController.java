d689ae6fd3def13322624d423e7641d4e8c4a66f
pamelamoreiras <pamelamoreiras99@gmail.com>
2023-06-19 15:27:33 -0300

    First Commit

---
+++ src/main/java/com/pamelamoreiras/bookstoremanager/controller/BookController.java
@@ -0,0 +1,11 @@
+package com.pamelamoreiras.bookstoremanager.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+
+
+}
