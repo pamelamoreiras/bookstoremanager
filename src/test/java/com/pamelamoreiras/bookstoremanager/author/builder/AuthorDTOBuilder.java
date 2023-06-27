d48565e6f53edd9de51bbe1b6fdacedff3c7e48c
pamelamoreiras <pamelamoreiras99@gmail.com>
2023-06-27 15:24:00 -0300

    Created test structure class

---
+++ src/test/java/com/pamelamoreiras/bookstoremanager/author/builder/AuthorDTOBuilder.java
@@ -0,0 +1,21 @@
+package com.pamelamoreiras.bookstoremanager.author.builder;
+
+import com.pamelamoreiras.bookstoremanager.author.dto.AuthorDTO;
+import lombok.Builder;
+
+@Builder
+public class AuthorDTOBuilder {
+
+    @Builder.Default
+    private final Long id = 1L;
+
+    @Builder.Default
+    private final String name = "Pamela Moreira";
+
+    @Builder.Default
+    private final int age = 23;
+
+    public AuthorDTO buidAuthorDTO() {
+        return new AuthorDTO(id, name, age);
+    }
+}
