c8e2a5609646609496ca7b95c1b22efa474aee52
pamelamoreiras <pamelamoreiras99@gmail.com>
2023-07-06 14:52:54 -0300

    Added roles support for authorization

--- src/main/java/com/pamelamoreiras/bookstoremanager/users/dto/UserDTO.java
+++ src/main/java/com/pamelamoreiras/bookstoremanager/users/dto/UserDTO.java
@@ -2,10 +2,12 @@ package com.pamelamoreiras.bookstoremanager.users.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.pamelamoreiras.bookstoremanager.users.enums.Gender;
+import com.pamelamoreiras.bookstoremanager.users.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.validation.constraints.Max;
@@ -49,4 +51,8 @@ public class UserDTO {
     @NotNull
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private LocalDate birthdate;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
--- src/main/java/com/pamelamoreiras/bookstoremanager/users/entity/User.java
+++ src/main/java/com/pamelamoreiras/bookstoremanager/users/entity/User.java
@@ -3,6 +3,7 @@ package com.pamelamoreiras.bookstoremanager.users.entity;
 import com.pamelamoreiras.bookstoremanager.books.entity.Book;
 import com.pamelamoreiras.bookstoremanager.entity.Auditable;
 import com.pamelamoreiras.bookstoremanager.users.enums.Gender;
+import com.pamelamoreiras.bookstoremanager.users.enums.Role;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -41,4 +42,8 @@ public class User extends Auditable {
 
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     private List<Book> books;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private Role role;
 }
